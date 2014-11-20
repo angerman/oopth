5f65104a87d9446b0e3aec0e00e2cc48c5cc9cbd
Moritz Angermann <moritz@lichtzwerge.de>
2014-11-20 12:23:38 +0100

    Adjusted ThCargo do produce the proper Type.
    Quasi m => m Exp failes during core lint.

--- ThCargo.hs
+++ ThCargo.hs
@@ -1,14 +1,15 @@
 {-# LANGUAGE Rank2Types #-}
 module ThCargo where
 import Foreign.StablePtr
+import Language.Haskell.TH.Lib ( ExpQ )
 import Language.Haskell.TH.Syntax ( Quasi, Exp(LitE), Lit(StringL) {-Exp(ConE), mkName -})
 
 foreign export ccall "getAction" getAction :: IO (StablePtr Action)
 
-data Action = QuasiAction  (Quasi m => m Exp)
+data Action = QuasiAction  (ExpQ)
 
 getAction :: IO (StablePtr Action)
-getAction = newStablePtr $ QuasiAction (return shippedSplice)
+getAction = newStablePtr $ QuasiAction shippedSplice
 
-shippedSplice :: Exp
-shippedSplice = LitE $ StringL "Nothing"
+shippedSplice :: ExpQ
+shippedSplice = return $ LitE (StringL "Nothing")
