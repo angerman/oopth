0f7bcbbfbf004dcfab932322736a992913d5f480
Moritz Angermann <moritz@lichtzwerge.de>
2014-11-25 21:27:10 +0100

    Make Out-Of-Proc (Standalone) work with cloud-haskell.

--- OutOfProcTH.hs
+++ OutOfProcTH.hs
@@ -32,6 +32,14 @@ import PprCore (pprCoreExpr)
 -- printing
 import Outputable
 
+-- running the runner
+import System.Process (createProcess, cwd, proc, waitForProcess)
+import System.Exit (ExitCode(..))
+import System.IO (hFlush, stdout)
+-- reading the value from the file
+import Binary
+import Serialized
+
 installOOPTHHook :: DynFlags -> DynFlags
 installOOPTHHook dyflags = dyflags { hooks = addHooks (hooks dyflags) }
   where
@@ -43,12 +51,37 @@ myResult = unsafeCoerce myExampleExpr
 myExampleExpr :: Q Exp
 myExampleExpr = return (ConE (mkName "Nothing"))
 
+readValueFromFile :: FilePath -> IO (Q Exp)
+readValueFromFile f = do
+  h   <- readBinMem f
+  dat <- get h
+  let Just value = fromSerialized deserializeWithData dat :: Maybe Exp
+  return . return $ value
+
+readHValueFromRunner :: FilePath -> IO HValue
+readHValueFromRunner fin = do
+  let base   = "/Users/angerman/Projects/haskell.org/oopth/"
+      runner = base ++ "Runner2"
+      fin'   = base ++ fin
+      fout   = fin' ++ ".result"
+      args   = [fin', fout]
+  putStrLn $ "Creating Process: " ++ runner ++ " with args " ++ (show args)
+  hFlush stdout
+  (_,_,_,h) <- createProcess (proc runner args) -- { cwd = mdir }
+  ec <- waitForProcess h
+  putStrLn $ "Done!"
+  case ec of
+    ExitSuccess -> unsafeCoerce $ readValueFromFile fout
+    ExitFailure i -> panic $ "Failed to run runner! " ++ show i
+
 -- most of this is an adapation from ghcjsCompileCoreExpr (Gen2.TH from ghcjs)
 compileCoreExpr :: HscEnv -> SrcSpan -> CoreExpr -> IO HValue
 compileCoreExpr hsc_env srcspan ds_expr = do
   -- see HscMain.hs:1653 for the default method.
   putStrLn "=== Compile Core Expr ==="
   buildDynamicLib dflags ds_expr
+  putStrLn "=== Contacting Runner ==="
+  result <- readHValueFromRunner "a.out"
 --  putStrLn $ showPpr dflags ds_expr
 --  prep_expr <- corePrepExpr dflags hsc_env ds_expr
 --  putStrLn $ showSDoc dflags (pprCoreExpr prep_expr)
@@ -66,7 +99,7 @@ compileCoreExpr hsc_env srcspan ds_expr = do
   --  2. Turn ce into a .dylib
   --  3. rund and execute the .dylib
   --  4. return the result.
-  return myResult
+  return result
   where
 --    isNonQ   = show ty == "GHC.Desugar.AnnotationWrapper"
 --    symb n   = "h$thrunnerZCThRunner" <> T.pack (show n) <> "zithExpr"
